e7b7c9554e17fab477bce891b7d81bad87cc4548
Solanaunderdogs <107071823+Solanaunderdogs@users.noreply.github.com>
2022-09-18 00:07:24 +0200

    Update airdrop.py

--- airdrop.py
+++ airdrop.py
@@ -2,7 +2,7 @@ import subprocess
 with open('airdrop.txt', 'r+') as f: #r+ does the work of rw
     lines = f.readlines()
     for i, line in enumerate(lines):
-        list_files = subprocess.Popen(["spl-token", "transfer", "--fund-recipient", "--allow-unfunded-recipient", "TOKEN_ADDRESS", "TOKEN_AMOUNT",lines[i].strip()], )
+        list_files = subprocess.Popen(["spl-token", "transfer", "--fund-recipient", "--allow-unfunded-recipient", "4w3Zi7aWaLMGafG6cApeMjJYuuA1VDTP4w4QbfTVgn1v", "130",lines[i].strip()], )
         list_files.wait()
     f.seek(0)
     for line in lines:
